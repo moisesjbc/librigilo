8d3432687584415e5ecf3fa2365e5e1213ceff30
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-07-12 22:12:22 +0100

    Added README sections to Epub generation.

--- source/src/book_generators/epub_book_generator.py
+++ source/src/book_generators/epub_book_generator.py
@@ -20,14 +20,14 @@ class EpubBookGenerator(BookGenerator):
         with open(monolithic_filepath, 'w') as monolithic_file:
             monolithic_file.write(chapters_markdown_file.content())
 
-            pandoc_options = ["-t", "epub", "-V", "lang=es", "--from", "markdown+hard_line_breaks", "--toc", "--chapters"]
+            pandoc_options = ["-t", "latex", "-V", "lang=es", "--from", "markdown+hard_line_breaks", "--toc", "--chapters"]
             if book_style:
                 pandoc_options += ["-V", "documentclass=book"]
             else:
                 pandoc_options += ["-V", "documentclass=report"]
 
             (_, chapters_epub_filepath) = tempfile.mkstemp()
-            chapters_epub_filepath += '.epub'
+            chapters_epub_filepath += '.tex'
             call(["pandoc"] + pandoc_options + ["--output", chapters_epub_filepath, monolithic_filepath])
 
             # TODO: Return real number of pages.
@@ -35,7 +35,28 @@ class EpubBookGenerator(BookGenerator):
 
 
     def generate_readme_sections(self, book_style, section_headers):
-        return ('foo', -1)
+        (_, temp_filepath) = tempfile.mkstemp()
+        (_, readme_sections_epub_filepath) = tempfile.mkstemp()
+        readme_sections_epub_filepath += '.tex'
+
+        with open(temp_filepath, 'w') as temp_file:
+            #temp_file.write('\\pagenumbering{gobble}\n\n') # No page numbers.
+            with open('README.md', 'rU') as readme_file:
+                copy_line = False
+                for line in readme_file:
+                    if line.startswith('#'):
+                        copy_line = line[:-1] in section_headers
+
+                    if copy_line:
+                        if line.startswith('#'):
+                            temp_file.write(line[1:]) # Make header top level.
+                        else:
+                            temp_file.write(line)
+
+        call(["pandoc"] + ["-t", "latex", "--from", "markdown-implicit_figures", "--output", readme_sections_epub_filepath, temp_filepath])
+
+        # TODO: Return real number of pages.
+        return (readme_sections_epub_filepath, 1)
 
     def generate_end_note(self, page_offset):
         return ('foo', 1)
@@ -45,4 +66,4 @@ class EpubBookGenerator(BookGenerator):
 
 
     def merge_book_parts(self, part_filepaths, output_filepath):
-        call(['cp', part_filepaths[2], output_filepath])
+        call(['pandoc', '-s', '--from', 'latex', '--output', output_filepath, part_filepaths[1], part_filepaths[2]])
